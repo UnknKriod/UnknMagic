334281d50fe44370eeb67803d1411f4ed734f478
UnknKriod <dirkabublika@gmail.com>
2026-05-23 17:10:49 +0300

    chore: Вынос версии и кода версии в gradle.properties

--- UnknMagic/app/build.gradle.kts
+++ UnknMagic/app/build.gradle.kts
@@ -1,4 +1,3 @@
-import se.bjurr.gitchangelog.plugin.gradle.GitChangelogTask
 import java.util.Properties
 
 plugins {
@@ -25,8 +24,10 @@ android {
         applicationId = "me.unknkriod.ang"
         minSdk = 24
         targetSdk = 36
-        versionCode = 727
-        versionName = "2.1.7"
+        versionCode = providers.gradleProperty("version-code")
+            .get()
+            .toInt()
+        versionName = property("version") as String
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
@@ -80,6 +81,7 @@ android {
             }
 
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
